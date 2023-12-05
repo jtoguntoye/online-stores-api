0bc8b4c99359426ac8d9a12029dbff46ad675331
Joel Oguntoye <joeloguntoye@gmail.com>
2023-12-05 14:25:29 +0100

    Separated stores and items data model

---
+++ db.py
@@ -0,0 +1,15 @@
+stores = {
+    "1": {"name": "store1"},
+    "2": {}    
+}
+
+items = {
+    "1": {
+        "name": "Chair",
+        "price": 17.99
+    },
+    "2": {
+        "name": "Table",
+        "price": 180.50
+    }   
+}
